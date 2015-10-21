6a332d0877fd85f87cfc8909088d6072d94c8a32
sergiomieic <sergio.mieic@gmail.com>
2015-10-22 00:38:22 +0100

    stuffing

--- stuffing.c
+++ stuffing.c
@@ -1,9 +1,25 @@
 #include "macros.h"
 
-
-int stuffing(){
+//recebe buffer CAMPO DE DADOS
+int write_stuffing(int fd, const char * buffer){
 	
+	int length = strlen(buffer);
 	
+	int i;
+	for(i=0; i < length; i++){		
+		
+		if(buffer[i] == FLAG){
+			write(fd,ESCAPE,1);
+			write(fd,FLAG_STUFFING,1);
+						
+		}else if(buffer[i] == ESCAPE){
+			write(fd,ESCAPE,1);
+			write(fd,ESCAPE_STUFFING,1);
+			
+		}else {
+			write(fd,buffer[i],1);
+		}		
+	}
 	
 	
 	
@@ -11,7 +27,7 @@ int stuffing(){
 	return 0;
 }
 
-int destuffing(){
+int read_destuffing(int fd){
 	
 	
 	
---
+++ stuffing.h
@@ -0,0 +1,8 @@
+#ifndef _STUFFING_H_
+#define _STUFFING_H_
+
+int write_stuffing(int fd, const char * buffer);
+int read_destuffing(int fd);
+
+
+#endif
